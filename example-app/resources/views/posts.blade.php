19c8c89065bfadb046d9c6526f6a8ae5b1bdd19b
matt190589 <matthew.doherty97@gmail.com>
2023-06-04 12:42:44 +0100

    File based db set up within Post class using meta data and collections

--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -6,7 +6,12 @@
 <body>
     <?php foreach ($posts as $post) : ?>
     <article>
-        <?= $post; ?>
+        <a href="/posts/<?= $post->slug; ?>">
+            <h1><?= $post->title; ?></h1>
+        </a>
+        <div>
+            <?= $post->body; ?>
+        </div>
     </article>
     <?php endforeach; ?>
 </body>
