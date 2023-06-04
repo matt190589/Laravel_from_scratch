3bdc9615ef4f15e86dca2acb7a3660cc6c423a27
matt190589 <matthew.doherty97@gmail.com>
2023-06-04 22:18:17 +0100

    Eloquent relationship set up between posts and categories table

--- example-app/app/Models/Post.php
+++ example-app/app/Models/Post.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'excerpt', 'body'];
+    // protected $fillable = ['title', 'excerpt', 'body', 'category_id'];
+    protected $guarded = [];
     //can also use $guarded which is the opposite of $fillable. Every is available unless stated.
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -6,6 +6,10 @@
            {!! $post->title !!}
         </a>
         </h1>
+
+        <p>
+            <a href="#">{{$post->category->name}}</a>
+        </p>
         <div>
            {{ $post->excerpt}}
         </div>
