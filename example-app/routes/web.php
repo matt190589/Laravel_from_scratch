d67396d83cc4871561577a0d20aaba46c3e9bcf4
matt190589 <matthew.doherty97@gmail.com>
2023-06-05 21:50:00 +0100

    Categories root added and home page SQL queries rewritten

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -18,7 +19,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
@@ -26,5 +27,11 @@ Route::get('posts/{post:slug}', function (Post $post) {
     // Find a post by its slug and pass it to a view called 'post'
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
