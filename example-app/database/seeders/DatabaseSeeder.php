fde2e2c813aec5b02f323f8d0cc66a39f70a3141
matt190589 <matthew.doherty97@gmail.com>
2023-06-05 22:38:47 +0100

    DatabaseSeeder file generated and DB seeded

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $fun = Category::create([
+            'name' => 'Fun',
+            'slug' => 'fun'
+        ]);
+
+        $professional = Category::create([
+            'name' => 'Professional',
+            'slug' => 'professional'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $fun->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p>All about my wicked family and their vibe</p>',
+            'body' => '<p>What a lovely bunch of coconuts. You need to meet them all to find out more</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $professional->id,
+            'title' => 'My Professional Post',
+            'slug' => 'my-second-post',
+            'excerpt' => '<p>All about my worklife</p>',
+            'body' => '<p>What a lovely bunch of coconuts. You need to meet them all to find out more</p>'
+        ]);
     }
 }
--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -11,7 +11,7 @@
             <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a>
         </p>
         <div>
-           {{ $post->excerpt}}
+           {!! $post->excerpt !!}
         </div>
     </article>
     @endforeach
