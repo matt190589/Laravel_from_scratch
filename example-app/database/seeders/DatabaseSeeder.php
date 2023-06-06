49c459f9a8eb3a9174500709417b8b2501b019dc
matt190589 <matthew.doherty97@gmail.com>
2023-06-06 21:30:06 +0100

    Post and category factories created to seed data

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
 use App\Models\User;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,43 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Matt D'
         ]);
 
-        $fun = Category::create([
-            'name' => 'Fun',
-            'slug' => 'fun'
-        ]);
 
-        $professional = Category::create([
-            'name' => 'Professional',
-            'slug' => 'professional'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $fun->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-first-post',
-            'excerpt' => '<p>All about my wicked family and their vibe</p>',
-            'body' => '<p>What a lovely bunch of coconuts. You need to meet them all to find out more</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $professional->id,
-            'title' => 'My Professional Post',
-            'slug' => 'my-second-post',
-            'excerpt' => '<p>All about my worklife</p>',
-            'body' => '<p>What a lovely bunch of coconuts. You need to meet them all to find out more</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
